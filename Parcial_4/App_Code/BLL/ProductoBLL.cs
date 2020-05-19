8174f8dd91a389edf7b99d073fdb41f9d869a9ce
MiguelGuzmanMena <64236325+MiguelGuzmanMena@users.noreply.github.com>
2020-05-18 22:52:31 -0600

    ok

---
+++ Parcial_4/App_Code/BLL/ProductoBLL.cs
@@ -0,0 +1,47 @@
+﻿using ProductoDSTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+/// <summary>
+/// Descripción breve de ProductoBLL
+/// </summary>
+/// 
+public class ProductoBLL
+{
+    ProductoDS Producto = new ProductoDS();
+    public ProductoBLL()
+    {
+        
+    }
+
+    public static List<Producto> MostrarTodosProductos()
+    {
+        List<Producto> ListarProducto = new List<Producto>();
+        ProductoDSTableAdapters.ProductosTableAdapter adaptador = new ProductoDSTableAdapters.ProductosTableAdapter();
+        ProductoDS.ProductosDataTable tabla = adaptador.MostrarProductos();
+        //Llenar
+        foreach (ProductoDS.ProductosRow filas in tabla)
+        {
+            ListarProducto.Add(filaDTOProductos(filas));
+        }
+        return ListarProducto;
+        //REtornar
+    }
+
+    private static Producto filaDTOProductos(ProductoDS.ProductosRow filas)
+    {
+        Producto objproductos = new Producto();
+        objproductos.id_categoria = filas.id_categoria;
+        objproductos.id_producto = filas.id_producto;
+        objproductos.nombre_producto = filas.nombre_producto;
+        objproductos.precio_unitario = filas.precio_unitario;
+        objproductos.detalles = filas.detalles;
+        objproductos.codigo = filas.codigo;
+
+        return objproductos;
+
+    }
+}
---
+++ Parcial_4/App_Code/DTO/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Producto
+/// </summary>
+public class Producto
+{
+    public Producto()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+    public int id_producto { get; set; }
+    public string nombre_producto { get; set; }
+    public int id_categoria { get; set; }
+    public decimal precio_unitario { get; set; }
+    public string detalles { get; set; }
+    public string codigo { get; set; }
+
+
+}
