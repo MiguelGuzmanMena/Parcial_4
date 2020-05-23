009484172b01685eaadb98b3fa3e1d4c4de90784
MiguelGuzmanMena <64236325+MiguelGuzmanMena@users.noreply.github.com>
2020-05-23 00:45:12 -0600

    Ok

---
+++ Parcial_4/App_Code/DTO/PedidoDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de PedidoDetalle
+/// </summary>
+public class PedidoDetalle
+{
+    public PedidoDetalle()
+    {
+        
+    }
+    public int id_pedido { get; set; }
+    public int id_producto { get; set; }
+    public decimal precio_unitario { get; set; }
+    public int cantidad { get; set; }
+    public decimal descuento { get; set; }
+}
---
+++ Parcial_4/App_Code/DTO/Pedidos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Pedidos
+/// </summary>
+public class Pedidos
+{
+    public Pedidos()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+    public int id_pedido { get; set; }
+    public int id_cliente { get; set; }
+    public int id_empleado { get; set; }
+    public int id_transportista { get; set; }
+    public DateTime fecha_ordenado { get; set; }
+    public DateTime fecha_requerido { get; set; }
+    public DateTime fecha_entregado { get; set; }
+    public decimal costo_envio { get; set; }
+    public string codigo { get; set; }
+    
+}
